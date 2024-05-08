16883e4f40ffb01ac898b99b2d2d0decb977f906
João Gaidzinski <gaidzinski2110@gmail.com>
2024-05-08 14:54:58 -0300

    Testes Unitarios WfCompetenciaBL

--- siga-wf/src/test/java/br/gov/jfrj/siga/wf/bl/WfCompetenciaBLTest.java
+++ siga-wf/src/test/java/br/gov/jfrj/siga/wf/bl/WfCompetenciaBLTest.java
@@ -2,7 +2,9 @@ package br.gov.jfrj.siga.wf.bl;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
@@ -11,6 +13,9 @@ import br.gov.jfrj.siga.cp.bl.CpCompetenciaBL;
 import br.gov.jfrj.siga.cp.bl.CpConfiguracaoBL;
 import br.gov.jfrj.siga.cp.model.enm.CpSituacaoDeConfiguracaoEnum;
 import br.gov.jfrj.siga.cp.model.enm.ITipoDeConfiguracao;
+import br.gov.jfrj.siga.dp.CpOrgaoUsuario;
+import br.gov.jfrj.siga.dp.DpCargo;
+import br.gov.jfrj.siga.dp.DpFuncaoConfianca;
 import br.gov.jfrj.siga.dp.DpLotacao;
 import br.gov.jfrj.siga.dp.DpPessoa;
 import br.gov.jfrj.siga.wf.model.WfConfiguracaoCache;
@@ -20,32 +25,157 @@ import br.gov.jfrj.siga.wf.model.enm.WfTipoDeConfiguracao;
 import br.gov.jfrj.siga.cp.CpConfiguracao;
 
 public class WfCompetenciaBLTest {
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
-	public void test_returns_true_if_configuration_exists_and_situation_is_pode() {
+	public void podeInstanciarProcedimentoPorConfiguracaoTRUE() {
+		//Configurando PESSOA
+		DpCargo cargo = new DpCargo();
+		cargo.setNomeCargo("CARGO-TESTE");
+		cargo.setIdCargo(1l);
+		DpFuncaoConfianca funcaoConf = new DpFuncaoConfianca();
+		
 		DpPessoa titular = new DpPessoa();
+		titular.setCargo(new DpCargo());
+		titular.setFuncaoConfianca(funcaoConf);
+		
+		//Configurando LOTACAO TITULAR
 		DpLotacao lotaTitular = new DpLotacao();
+		CpOrgaoUsuario orgaoUsuario = new CpOrgaoUsuario();
+		lotaTitular.setOrgaoUsuario(orgaoUsuario);
+		
 		WfDefinicaoDeProcedimento definicaoDeProcedimento = new WfDefinicaoDeProcedimento();
 		definicaoDeProcedimento.setAcessoDeEdicao(WfAcessoDeEdicao.ACESSO_PUBLICO);
-
 		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
-
-		boolean result = wfCompetenciaBL.podeInstanciarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
-
-		Assert.assertTrue(result);
+		
+		Boolean result = wfCompetenciaBL.podeInstanciarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
+		
+		Assert.assertEquals(true, result);
 	}
-
+	
 	@Test
-	public void test_correctly_handles_null_values_for_titular_parameter() {
-		DpPessoa titular = null;
+	public void podeInstanciarProcedimentoPorConfiguracaoFALSE() {
+		//Configurando PESSOA
+		DpCargo cargo = new DpCargo();
+		cargo.setNomeCargo("CARGO-TESTE");
+		cargo.setIdCargo(1l);
+		DpFuncaoConfianca funcaoConf = new DpFuncaoConfianca();
+		
+		DpPessoa titular = new DpPessoa();
+		titular.setCargo(new DpCargo());
+		titular.setFuncaoConfianca(funcaoConf);
+		
+		//Configurando LOTACAO TITULAR
 		DpLotacao lotaTitular = new DpLotacao();
+		CpOrgaoUsuario orgaoUsuario = new CpOrgaoUsuario();
+		lotaTitular.setOrgaoUsuario(orgaoUsuario);
+		
+		WfDefinicaoDeProcedimento definicaoDeProcedimento = new WfDefinicaoDeProcedimento();
+		definicaoDeProcedimento.setAcessoDeEdicao(WfAcessoDeEdicao.ACESSO_PESSOAL);
+		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
+		
+		Boolean result = wfCompetenciaBL.podeInstanciarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
+		
+		Assert.assertEquals(true, result);
+	}
+	
+	@Test
+	public void podeInstanciarProcedimentoPorConfiguracaoNULOS() {
+		//Configurando PESSOA
+		DpPessoa titular = null;
+		
+		//Configurando LOTACAO TITULAR
+		DpLotacao lotaTitular = null;
+		
+		WfDefinicaoDeProcedimento definicaoDeProcedimento = new WfDefinicaoDeProcedimento();
+		definicaoDeProcedimento.setAcessoDeEdicao(WfAcessoDeEdicao.ACESSO_PESSOAL);
+		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
+		
+		Boolean result = wfCompetenciaBL.podeInstanciarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
+		
+		Assert.assertEquals(false, result);
+	}
+	
+	
+	@Test
+	public void podeEditarProcedimentoPorConfiguracaoTRUE() {
+		//Configurando PESSOA
+		DpCargo cargo = new DpCargo();
+		cargo.setNomeCargo("CARGO-TESTE");
+		cargo.setIdCargo(1l);
+		DpFuncaoConfianca funcaoConf = new DpFuncaoConfianca();
+		
+		DpPessoa titular = new DpPessoa();
+		titular.setCargo(new DpCargo());
+		titular.setFuncaoConfianca(funcaoConf);
+		
+		//Configurando LOTACAO TITULAR
+		DpLotacao lotaTitular = new DpLotacao();
+		CpOrgaoUsuario orgaoUsuario = new CpOrgaoUsuario();
+		lotaTitular.setOrgaoUsuario(orgaoUsuario);
+		
 		WfDefinicaoDeProcedimento definicaoDeProcedimento = new WfDefinicaoDeProcedimento();
 		definicaoDeProcedimento.setAcessoDeEdicao(WfAcessoDeEdicao.ACESSO_PUBLICO);
-
 		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
-
-		boolean result = wfCompetenciaBL.podeInstanciarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
-
-		Assert.assertFalse(result);
+		
+		Boolean result = wfCompetenciaBL.podeEditarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
+		
+		Assert.assertEquals(true, result);
 	}
+	
+	@Test
+	public void podeEditarProcedimentoPorConfiguracaoFALSE() {
+		//Configurando PESSOA
+		DpCargo cargo = new DpCargo();
+		cargo.setNomeCargo("CARGO-TESTE");
+		cargo.setIdCargo(1l);
+		DpFuncaoConfianca funcaoConf = new DpFuncaoConfianca();
+		
+		DpPessoa titular = new DpPessoa();
+		titular.setCargo(new DpCargo());
+		titular.setFuncaoConfianca(funcaoConf);
+		
+		//Configurando LOTACAO TITULAR
+		DpLotacao lotaTitular = new DpLotacao();
+		CpOrgaoUsuario orgaoUsuario = new CpOrgaoUsuario();
+		lotaTitular.setOrgaoUsuario(orgaoUsuario);
+		
+		WfDefinicaoDeProcedimento definicaoDeProcedimento = new WfDefinicaoDeProcedimento();
+		definicaoDeProcedimento.setAcessoDeEdicao(WfAcessoDeEdicao.ACESSO_PESSOAL);
+		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
+		
+		Boolean result = wfCompetenciaBL.podeEditarProcedimentoPorConfiguracao(titular, lotaTitular, definicaoDeProcedimento);
+		
+		Assert.assertEquals(true, result);
+	}
+	
+	
+//	@Test(expected = Exception.class)
+//	public void testaCompetencia1() {
+//		// Configurando PESSOA
+//		DpCargo cargo = new DpCargo();
+//		cargo.setNomeCargo("CARGO-TESTE");
+//		cargo.setIdCargo(1l);
+//		DpFuncaoConfianca funcaoConf = new DpFuncaoConfianca();
+//
+//		DpPessoa titular = new DpPessoa();
+//		titular.setCargo(new DpCargo());
+//		titular.setFuncaoConfianca(funcaoConf);
+//
+//		// Configurando LOTACAO TITULAR
+//		DpLotacao lotaTitular = new DpLotacao();
+//		CpOrgaoUsuario orgaoUsuario = new CpOrgaoUsuario();
+//		lotaTitular.setOrgaoUsuario(orgaoUsuario);
+//		
+//		WfCompetenciaBL wfCompetenciaBL = new WfCompetenciaBL();
+//		
+//		thrown.expect(Exception.class);
+//		try {
+//			wfCompetenciaBL.testaCompetencia("SegundoFatorPin", titular, lotaTitular);
+//		}catch(Exception e) {
+//			
+//		}
+//	}
 }
